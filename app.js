9ae8bd02157943376b7b7cf7d4de94138ed3a8f4
Fernando Passos <30850453+fernandodmp@users.noreply.github.com>
2020-02-29 09:49:46 -0300

    Adds a few security middlewares

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const logger = require('morgan');
 
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
 const rulesRouter = require('./routes/rulesRouter');
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 15 // limit each IP to 15 requests per windowMs
+});
+app.use(limiter);
+app.use(helmet());
 
 app.use(logger('dev'));
 app.use(express.json());
